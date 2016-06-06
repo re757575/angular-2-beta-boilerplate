8be7446e912e2299dd8b29b60e3f3a352fdc41aa
Alex.Dai <alex.dai@plusdi.com>
2016-06-06 15:21:28 +0800

    Added interpolation binding

--- dev/my-first.component.ts
+++ dev/my-first.component.ts
@@ -4,8 +4,13 @@ import {Component} from 'angular2/core';
    selector: 'my-first-component',
    template: `
         <h1>My First Component</h1>
+        <span>My name is {{name}} !</span>
    `
 })
 export class MyFirstComponent {
-    constructor() {}
+    name: string;
+
+    constructor() {
+        this.name = 'Alex';
+    }
 }
