fb1cd5fb88e0d9f1a8a272db2621878ef34035db
Alex.Dai <alex.dai@plusdi.com>
2016-06-06 16:54:40 +0800

    Modify:  style

--- dev/my-first.component.ts
+++ dev/my-first.component.ts
@@ -3,20 +3,21 @@ import {Component} from 'angular2/core';
 @Component({
    selector: 'my-first-component',
    template: `
-        <h1>My First Component</h1>
-        <span>My name is {{name}} !</span>
-        <br/>
-        <input [(ngModel)]="name" placeholder="type your name"/>
-        <br/>
-        <input [value]="name" [placeholder]="name === '' 
-            ? 'this is property binding'
-            : name" readonly="true"/>
-        <br/>
-        <button (click)="myClick()">click me</button>
-        
-        <br/><br/>
-        <input (keyup)="mykeyup(inputElement.value)" #inputElement placeholder="type something"/>
-        <p>{{values}}</p>
+        <section>
+            <h2>My First Component</h2>
+            <p>My name is <span [style.color]="'red'">{{name}}</span></p>
+            <br/>
+            <input [(ngModel)]="name" placeholder="type your name"/>
+            <br/>
+            <input [value]="name" [placeholder]="name === ''
+                ? 'this is property binding'
+                : name" readonly="true"/>
+            <br/>
+            <button (click)="myClick()">click me</button>
+            <br/>
+            <input (keyup)="mykeyup(inputElement.value)" #inputElement placeholder="type something"/>
+            <p>{{values}}</p>
+        </section>
    `
 })
 export class MyFirstComponent {
