1a5f5c2382c87ba44f5c6796f13b27d0d130677a
Alex.Dai <alex.dai@plusdi.com>
2016-06-06 15:14:07 +0800

    Added: first componemt

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,14 @@
 import {Component} from 'angular2/core';
+import {MyFirstComponent} from './my-first.component';
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
+        <my-first-component></my-first-component>
     `,
+    directives: [MyFirstComponent]
 })
 export class AppComponent {
 
---
+++ dev/my-first.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+   selector: 'my-first-component',
+   template: `
+        <h1>My First Component</h1>
+   `
+})
+export class MyFirstComponent {
+    constructor() {}
+}
